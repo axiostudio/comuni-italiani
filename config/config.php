fcd9480ac383fec037bdf6114b4ef9dc604a9d03
Andrea Rufo <a.rufo@axio.studio>
2025-02-26 09:56:23 +0100

    Revisione gestione import

--- config/config.php
+++ config/config.php
@@ -7,10 +7,10 @@ return [
         'api',
     ],
     'import' => [
-        'zip_codes_file' => 'https://raw.githubusercontent.com/axiostudio/comuni-italiani/main/data/import/json/gi_cap.json',
-        'province_codes_file' => 'https://raw.githubusercontent.com/axiostudio/comuni-italiani/main/data/import/json/gi_province.json',
-        'comuni_data_file' => 'https://raw.githubusercontent.com/axiostudio/comuni-italiani/main/data/import/json/gi_comuni.json',
-        'regioni_data_file' => 'https://raw.githubusercontent.com/axiostudio/comuni-italiani/main/data/import/json/gi_regioni.json',
-        'zone_data_file' => 'https://raw.githubusercontent.com/axiostudio/comuni-italiani/main/data/import/json/gi_zone.json',
+        'zone_data_file' =>  dirname(__DIR__).'/data/import/json/gi_zone.json',
+        'regioni_data_file' => dirname(__DIR__).'/data/import/json/gi_regioni.json',
+        'province_data_file' => dirname(__DIR__).'/data/import/json/gi_province.json',
+        'comuni_data_file' => dirname(__DIR__).'/data/import/json/gi_comuni.json',
+        'zip_data_file' => dirname(__DIR__).'/data/import/json/gi_cap.json',
     ],
 ];
